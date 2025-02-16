4729ffe6db9490a42f5a0081e89b091eeffd1bd6
Albert Skalt <askalt@tarantool.org>
2025-02-16 18:23:55 +0300

    ci: fix checkdiff

--- ci/scripts/checkdiff.sh
+++ ci/scripts/checkdiff.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Scripts checks a diff between two refs: $SRC, $DST,
+# Script checks a diff between two refs: $SRC, $DST,
 # looking for diff like $DIFF among changed files.
-#
-# Writes `changed` to the $OUT.
+# If diff is found returns 1.
 
 if git diff --name-only $SRC $DST | grep $DIFF; then
     echo "Sources are changed"
-    echo "changed=true" >> $OUT
+    exit 1
 else
     echo "Sources are unchanged"
-    echo "changed=false" >> $OUT
+    exit 0
 fi
